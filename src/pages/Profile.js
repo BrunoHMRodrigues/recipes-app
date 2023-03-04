e6ba1e60757afb98431823c981171c0724ef9c39
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-03-04 09:38:39 -0300

    Feature: Requisite 58

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+// localStorage.setItem('user', '{"email":"email@mail.com"}');
 function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
-      <p data-testid="profile-email" />
+      <p data-testid="profile-email">{user.email}</p>
       <button type="button" data-testid="profile-done-btn">Done Recipes</button>
       <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
       <button type="button" data-testid="profile-logout-btn">Logout</button>
