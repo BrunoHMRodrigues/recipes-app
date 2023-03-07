84c81406048eb2a69844d9714a6f545297cded93
Luisgustavo11 <luisgustavordp@gmail.com>
2023-03-07 18:26:34 -0300

    WIP: requisite 30 e 31

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import RecipeDetails from './pages/RecipeDetails';
-
+import RecipeInProgress from './pages/RecipeInProgress';
 import DoneRecipes from './pages/DoneRecipes';
 import Profile from './pages/Profile';
 import FavoriteRecipes from './pages/FavoriteRecipes';
@@ -16,6 +16,8 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/meals/:idReceita" component={ RecipeDetails } />
         <Route path="/drinks/:idReceita" component={ RecipeDetails } />
+        <Route path="/meals/:idReceita/in-progress" component={ RecipeInProgress } />
+        <Route path="/drinks/:idReceita/in-progress" component={ RecipeInProgress } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/profile" component={ Profile } />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
